9a6f6dfd118baa5aa1176af385f17ec7bad598b4
Yoshinobu <Yoshinobu>
2020-07-06 13:45:27 +0900

    create bookListViewModel

---
+++ BookShelf/ViewModels/BookListViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  BookListViewModel.swift
+//  BookShelf
+//
+//  Created by 村尾慶伸 on 2020/07/06.
+//  Copyright © 2020 村尾慶伸. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+class BookListViewModel: ObservableObject {
+    @Published var bookCellViewModels = [BookCellViewModel]()
+
+    private var cancellables = Set<AnyCancellable>()
+
+    init() {
+        self.bookCellViewModels = testDataBooks.map { book in
+            BookCellViewModel(book: book)
+        }
+    }
+
+}
